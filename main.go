27b7a70478059023e0e32b7ccb260d072e20c625
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2026-09-21 18:53:51 +0100

    Set up basic snippet handlers

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home) // Restrict the route to exact matches of the "/" path.
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
